c40c4fc3d982e38007837aacfd1306af5b845959
Vladimir Likic <v.likic1@gmail.com>
2021-12-03 21:30:41 +1100

    Updated 'test_0001'

--- tests/test_0001/test_0001.py
+++ tests/test_0001/test_0001.py
@@ -7,7 +7,7 @@ this_folder = os.path.dirname(__file__)
 def test_base_structure():
     mm = metamodel_for_language('usysml')
     model = mm.model_from_file(os.path.join(this_folder,
-                                            'VehicleExample0001.sysml'))
+                                            'test0001.sysml'))
 
     assert model.elements[0].name == 'PackageVehicles'
     assert type(model.elements[0]).__name__ == 'Package'
