fa6968e2e424df5bedec75701af55846cbc65cd8
Vladimir Likic <v.likic1@gmail.com>
2021-12-04 20:16:17 +1100

    Added 'test_0003'

---
+++ tests/test_0003/test_0003.py
@@ -0,0 +1,17 @@
+import os
+from textx import metamodel_for_language
+
+this_folder = os.path.dirname(__file__)
+
+
+def test_usysml():
+    mm = metamodel_for_language('usysml')
+    model = mm.model_from_file(os.path.join(this_folder,
+                                            'test0003.sysml'))
+
+    assert model.elements[0].name == 'PackageVehicles'
+    assert type(model.elements[0]).__name__ == 'Package'
+
+    e = model.elements[0].elements[1]
+    assert e.name == 'Wheel'
+    assert type(e).__name__ == 'PartDef'
