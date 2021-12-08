3e90d2571d7089cb98350a46d871133ec6d38238
Vladimir Likic <v.likic1@gmail.com>
2021-12-08 13:11:03 +1100

    Amended test_0008

---
+++ tests/test_0008/test_0008.py
@@ -0,0 +1,21 @@
+import os
+from textx import metamodel_for_language
+
+this_folder = os.path.dirname(__file__)
+
+
+def test_usysml():
+    mm = metamodel_for_language('usysml')
+    model = mm.model_from_file(os.path.join(this_folder,
+                                            'test0008.sysml'))
+
+    assert model.elements[0].name == 'PackageVehicles'
+    assert type(model.elements[0]).__name__ == 'Package'
+
+    e = model.elements[0].elements[7]
+    assert e.name == 'partnotype1'
+    assert e.type is None
+
+    e = model.elements[0].elements[-1].elements[0]
+    assert e.name == 'partnotype2'
+    assert e.type is None
