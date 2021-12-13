8596db8c22a4818442f3d3651d08e2b81722db94
Vladimir Likic <v.likic1@gmail.com>
2021-12-14 08:09:44 +1100

    Removed the starting '.' from the namespace paths in test_0001

--- tests/test_0001/test_0001.py
+++ tests/test_0001/test_0001.py
@@ -10,18 +10,18 @@ def test_usysml():
     model = mm.model_from_file(os.path.join(this_folder,
                                             'test0001.sysml'))
 
-    e = get_element_by_name(model, '.PackageVehicles')
+    e = get_element_by_name(model, 'PackageVehicles')
     assert type(e).__name__ == 'Package'
 
-    e = get_element_by_name(model, '.PackageVehicles.Vehicle')
+    e = get_element_by_name(model, 'PackageVehicles.Vehicle')
     assert type(e).__name__ == 'PartDef'
 
-    e1 = get_element_by_name(model, '.PackageVehicles.Wheel')
+    e1 = get_element_by_name(model, 'PackageVehicles.Wheel')
     assert type(e1).__name__ == 'PartDef'
 
-    e = get_element_by_name(model, '.PackageVehicles.vehicle')
+    e = get_element_by_name(model, 'PackageVehicles.vehicle')
     assert type(e).__name__ == 'Part'
 
-    e = get_element_by_name(model, '.PackageVehicles.vehicle.w')
+    e = get_element_by_name(model, 'PackageVehicles.vehicle.w')
     assert type(e).__name__ == 'Part'
     assert e.type is e1
