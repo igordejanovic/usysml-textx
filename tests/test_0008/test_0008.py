af43bf1d6ce32aec398e1e61d77648d1cb7b5340
Vladimir Likic <v.likic1@gmail.com>
2021-12-15 13:13:59 +1100

    Updated test_008

--- tests/test_0008/test_0008.py
+++ tests/test_0008/test_0008.py
@@ -1,20 +1,24 @@
 import os
 from textx import metamodel_for_language
 from usysml.utils import get_element_by_name
+from usysml.utils import assert_element_classfr, assert_element_type
 
-this_folder = os.path.dirname(__file__)
+test_case = os.path.dirname(__file__)
 
 
 def test_usysml():
     mm = metamodel_for_language('usysml')
-    model = mm.model_from_file(os.path.join(this_folder,
+    model = mm.model_from_file(os.path.join(test_case,
                                             'test0008.sysml'))
 
-    e = get_element_by_name(model, 'PackageVehicles')
-    assert type(e).__name__ == 'Package'
+    assert_element_classfr(model, 'PackageVehicles', 'Package')
+    assert_element_classfr(model, 'PackageVehicles.Vehicle', 'PartDef')
+    assert_element_classfr(model, 'PackageVehicles.Wheel', 'PartDef')
+    assert_element_classfr(model, 'PackageVehicles.partnotype1', 'Part')
+    assert_element_classfr(model, 'PackageVehicles.vehicle', 'Part')
+    assert_element_classfr(model, 'PackageVehicles.vehicle.w', 'Part')
+    assert_element_classfr(model, 'PackageVehicles.vehicle.partnotype2',
+                           'Part')
 
-    e = get_element_by_name(model, 'PackageVehicles.partnotype1')
-    assert e.type is None
-
-    e = get_element_by_name(model, 'PackageVehicles.vehicle.partnotype2')
-    assert e.type is None
+    assert_element_type(model, 'PackageVehicles.partnotype1', 'None')
+    assert_element_type(model, 'PackageVehicles.vehicle.partnotype2', 'None')
