017fc8457399e295e4d8778b37fa5b9c3172fc83
Vladimir Likic <v.likic1@gmail.com>
2021-12-15 12:57:32 +1100

    Updated test_0005

--- tests/test_0005/test_0005.py
+++ tests/test_0005/test_0005.py
@@ -1,17 +1,40 @@
 import os
 from textx import metamodel_for_language
 from usysml.utils import get_element_by_name
+from usysml.utils import assert_element_name, assert_element_type
 
-this_folder = os.path.dirname(__file__)
+test_case = os.path.dirname(__file__)
 
 
 def test_usysml():
     mm = metamodel_for_language('usysml')
-    model = mm.model_from_file(os.path.join(this_folder,
+    model = mm.model_from_file(os.path.join(test_case,
                                             'test0005.sysml'))
 
-    e = get_element_by_name(model, 'PackageVehicles')
-    assert type(model.elements[0]).__name__ == 'Package'
+    assert_element_name(model, 'PackageVehicles', 'Package')
+    assert_element_name(model, 'PackageVehicles.Vehicle', 'PartDef')
+    assert_element_name(model, 'PackageVehicles.Wheel', 'PartDef')
+    assert_element_name(model, 'PackageVehicles.WheelAxle', 'PartDef')
+    assert_element_name(model, 'PackageVehicles.test_vehicle', 'Part')
+    assert_element_name(model, 'PackageVehicles.vehicle', 'Part')
+    assert_element_name(model, 'PackageVehicles.vehicle.a', 'Part')
+    assert_element_name(model, 'PackageVehicles.vehicle.w', 'Part')
+    assert_element_name(model, 'PackageVehicles.vehicle.w.LugBolt', 'PartDef')
+    assert_element_name(model, 'SupportComponents', 'Package')
+    assert_element_name(model, 'SupportComponents.parking_space', 'Part')
+    assert_element_name(model, 'SupportComponents.vehicle_shed', 'Part')
+    assert_element_name(model, 'SupportComponents.repair_shop', 'Part')
+    assert_element_name(model, 'SupportComponents.repair_shop.VehicleLift',
+                        'PartDef')
 
-    e = get_element_by_name(model, 'Wheel')
-    assert type(e).__name__ == 'PartDef'
+    assert_element_type(model, 'PackageVehicles.test_vehicle',
+                        'PackageVehicles.Vehicle')
+    assert_element_type(model, 'PackageVehicles.vehicle',
+                        'PackageVehicles.Vehicle')
+    assert_element_type(model, 'PackageVehicles.vehicle.a',
+                        'PackageVehicles.WheelAxle')
+    assert_element_type(model, 'PackageVehicles.vehicle.w',
+                        'PackageVehicles.Wheel')
+    assert_element_type(model, 'SupportComponents.parking_space', 'None')
+    assert_element_type(model, 'SupportComponents.vehicle_shed', 'None')
+    assert_element_type(model, 'SupportComponents.repair_shop', 'None')
