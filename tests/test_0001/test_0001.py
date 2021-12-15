a2a9fc7619e392b1129e3fd4164a8e2a541a27a1
Vladimir Likic <v.likic1@gmail.com>
2021-12-15 13:03:56 +1100

    Updated all tests for 'assert_element_classfr()'

--- tests/test_0001/test_0001.py
+++ tests/test_0001/test_0001.py
@@ -1,7 +1,7 @@
 import os
 from textx import metamodel_for_language
 from usysml.utils import get_element_by_name
-from usysml.utils import assert_element_name, assert_element_type
+from usysml.utils import assert_element_classfr, assert_element_type
 
 test_case = os.path.dirname(__file__)
 
@@ -11,11 +11,11 @@ def test_usysml():
     model = mm.model_from_file(os.path.join(test_case,
                                             'test0001.sysml'))
 
-    assert_element_name(model, 'PackageVehicles', 'Package')
-    assert_element_name(model, 'PackageVehicles.Vehicle', 'PartDef')
-    assert_element_name(model, 'PackageVehicles.Wheel', 'PartDef')
-    assert_element_name(model, 'PackageVehicles.vehicle', 'Part')
-    assert_element_name(model, 'PackageVehicles.vehicle.w', 'Part')
+    assert_element_classfr(model, 'PackageVehicles', 'Package')
+    assert_element_classfr(model, 'PackageVehicles.Vehicle', 'PartDef')
+    assert_element_classfr(model, 'PackageVehicles.Wheel', 'PartDef')
+    assert_element_classfr(model, 'PackageVehicles.vehicle', 'Part')
+    assert_element_classfr(model, 'PackageVehicles.vehicle.w', 'Part')
 
     assert_element_type(model, 'PackageVehicles.vehicle.w',
                         'PackageVehicles.Wheel')
